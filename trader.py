26ee489c26b996333fc69cf5f2ffff177e628315
Ali Kamel <alik87932@gmail.com>
2022-06-12 12:27:27 +0100

    removed TODO

--- trader.py
+++ trader.py
@@ -18,7 +18,6 @@ from reinforcement import Environment as RL
 #TODO: stocks data folder should have a file which contains metadata specifically abt how much of the data the neural network has already been trained on
 #TODO: save neural network so that you don't have to retrain it on the whole dataset everytime - this means that the rest of the dataset will also be "internalised" each time
 #TODO: consider saving standard scaler parameters as well so that we don't have to keep old dataset in order to standard scale the new data every time
-#TODO: consider changing trade() so that if prediction is none and there is an open position, we keep the position
 #TODO: add wifi-down feature that keeps hte application open if there's no internet connection
 #TODO: improve tws disconnection management.
 class Trader:
